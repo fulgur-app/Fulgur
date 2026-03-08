23d8b6a6a70912260f15e6da9f4cff4acb81510b
PRRPCHT <pierrepic@gmail.com>
2026-03-08 06:21:23 +0100

    Adding Pascal/Delphi/Freepascal syntax highlighting support to the code editor

---
+++ src/fulgur/languages/syntax_highlighting/pascal.rs
@@ -0,0 +1,32 @@
+use gpui_component::highlighter::{LanguageConfig, LanguageRegistry};
+use tree_sitter_pascal;
+
+const HIGHLIGHTS_QUERY: &str = include_str!("queries/pascal_highlights.scm");
+
+/// Add Pascal / Delphi / FreePascal syntax highlighting support.
+pub fn add_pascal_support() {
+    LanguageRegistry::singleton().register(
+        "pascal",
+        &LanguageConfig::new(
+            "pascal",
+            tree_sitter_pascal::LANGUAGE.into(),
+            vec![],
+            HIGHLIGHTS_QUERY,
+            "",
+            "",
+        ),
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add_pascal_support_registers_language() {
+        super::add_pascal_support();
+        assert!(
+            gpui_component::highlighter::LanguageRegistry::singleton()
+                .language("pascal")
+                .is_some()
+        );
+    }
+}
