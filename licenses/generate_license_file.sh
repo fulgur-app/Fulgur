cf051a662e32e27a9f8f4ba9da823f7ff29cfe2d
PRRPCHT <pierrepic@gmail.com>
2026-03-17 23:48:56 +0100

    chore(licenses): moved licenses script, fix release workflow, and removed third-party licenses from repo (generated on release)

--- licenses/generate_license_file.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-cd ..
-cargo about generate licenses/about.hbs -c licenses/about.toml --workspace -o THIRD-PARTY-LICENSES
---
+++ scripts/generate_license_file.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+cargo about generate licenses/about.hbs -c licenses/about.toml --workspace -o THIRD-PARTY-LICENSES
