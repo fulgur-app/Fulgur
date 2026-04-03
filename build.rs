64de0649519c6b8685ea92b29f3541d1257e9bd8
PRRPCHT <pierrepic@gmail.com>
2026-04-03 10:18:04 +0200

    feat(windows): add separate file icon for file associations via custom NSIS template

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
+        println!("cargo:rerun-if-changed=assets/icon.ico");
+        println!("cargo:rerun-if-changed=assets/file_icon.ico");
+        println!("cargo:rerun-if-changed=resources/windows/app.rc");
         // Embed the icon resource on Windows
         let _ = embed_resource::compile("resources/windows/app.rc", embed_resource::NONE);
     }
